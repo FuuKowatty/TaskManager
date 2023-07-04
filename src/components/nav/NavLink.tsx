2982a72e3d2226e9427d51c9ae29810be36f6474
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-07-04 12:01:02 +0200

    feat: add mobile navbar

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -18,16 +18,16 @@ export function NavLink({ href, text, icon: Icon }: Props) {
   const activeClass = isActive && "font-bold text-white";
 
   return (
-    <li className="w-full">
+    <li className="flex w-full items-center justify-center">
       <Link
         href={href}
         className={clsx(
-          "flex w-full items-center  gap-4 rounded-lg p-3 text-gray-400 transition-all hover:font-bold hover:text-white focus:font-bold focus:text-white focus:outline-none focus:ring-2 focus:ring-gray-50",
+          "flex w-full items-center justify-center rounded-lg text-gray-400 transition-all hover:font-bold hover:text-white focus:font-bold focus:text-white focus:outline-none focus:ring-2 focus:ring-gray-50 lg:justify-start lg:gap-4 lg:p-3",
           activeClass
         )}
       >
-        <Icon className="h-7 w-7" />
-        {text}
+        <Icon className="h-5 w-5 lg:h-7 lg:w-7" />
+        <span className="visually-hidden lg:visually-visible">{text}</span>
       </Link>
     </li>
   );
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -37,19 +37,18 @@ const navData: NavData = [
 
 export function Navbar() {
   return (
-    <nav className="my-7 ml-7 flex w-64 flex-shrink-0 flex-col rounded-3xl bg-slate-950 py-14 pl-8 pr-12">
+    <nav className="fixed bottom-2 left-0 right-0 mx-4 flex flex-shrink-0 gap-4 rounded-xl bg-slate-950  px-6 py-3 lg:static lg:my-7 lg:ml-7 lg:w-64 lg:flex-col lg:gap-0 lg:rounded-3xl lg:py-14 lg:pl-8 lg:pr-12">
       <Link
         href="/"
-        className="mb-24 ml-2 inline-block rounded-lg p-2 font-golos-text text-5xl font-black focus:outline-none focus:ring-2 focus:ring-gray-200"
+        className="mb-24 ml-2 hidden rounded-lg p-2 font-golos-text text-5xl font-black focus:outline-none focus:ring-2 focus:ring-gray-200 lg:inline-block"
       >
         Logo
       </Link>
-      <ul className="flex flex-col gap-14">
+      <ul className="flex flex-1 gap-4 lg:flex-col lg:gap-14">
         {navData.map(({ icon, text, href }) => {
           return <NavLink key={text} href={href} icon={icon} text={text} />;
         })}
       </ul>
-
       <DarkModeSwitcher />
     </nav>
   );
