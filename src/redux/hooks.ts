1b905e68047df893e9032a57e32a4288d9d92bc5
FuuKowatty <miedzio.2000@gmail.com>
2023-07-04 12:30:15 +0200

    fix hook  types

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,11 +1,7 @@
-import type { Action, ThunkAction } from "@reduxjs/toolkit";
 import type { TypedUseSelectorHook } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
 
-import type { AppDispatch, RootState } from "./store";
+import type { RootState } from "./store";
 
-export const useAppDispatch = () =>
-  useDispatch<
-    AppDispatch | ThunkAction<void, RootState, unknown, Action<string>>
-  >();
+export const useAppDispatch = () => useDispatch();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
