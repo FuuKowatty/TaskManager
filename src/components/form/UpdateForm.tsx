ba27964aef0d0b119e70833277d97a01be4ca834
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-07-23 22:20:31 +0200

    feat: add styling

---
+++ src/components/form/UpdateForm.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState } from "react";
+
+import { UpdateConfirmation } from "@/components/form/UpdateConfirmation";
+import { HashPasswordInput } from "@/components/HashPasswordInput";
+
+import { useUpdateUser } from "@/hooks/api/useUpdateUser";
+import { useSession } from "@/hooks/state/useSession";
+
+interface UpdateForm {
+  defaultValue: string;
+  id: number;
+  type: "email" | "password";
+}
+
+export function UpdateForm({ defaultValue, id, type }: UpdateForm) {
+  const { logout } = useSession();
+  const [isReadOnly, setIsReadOnly] = useState(true);
+  const [value, setValue] = useState(defaultValue);
+  const { mutate: handleUpdateUser } = useUpdateUser(id);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleUpdateUser(
+      { email: value },
+      {
+        onSuccess: () => {
+          setIsReadOnly(true);
+          logout();
+        },
+      }
+    );
+  };
+
+  return (
+    <form
+      className="flex max-w-[250px] flex-col gap-2 lg:max-w-[unset] lg:flex-row lg:items-end lg:gap-4"
+      onSubmit={handleSubmit}
+    >
+      {type === "email" && (
+        <label className="flex flex-col gap-1">
+          Update Email
+          <input
+            name="email"
+            type="email"
+            placeholder="JohnCruise@gmail.com"
+            className="min-w-[256px] rounded-sm p-2"
+            readOnly={isReadOnly}
+            value={isReadOnly ? defaultValue : value}
+            onChange={(event) => setValue(event.target.value)}
+          />
+        </label>
+      )}
+
+      {type === "password" && (
+        <label>
+          Password (Hashed)
+          <HashPasswordInput
+            value={isReadOnly ? defaultValue : value}
+            handleChange={(event) => setValue(event.target.value)}
+            readOnly={isReadOnly}
+            styled="settings"
+          />
+        </label>
+      )}
+      <UpdateConfirmation
+        isReadOnly={isReadOnly}
+        setIsReadOnly={setIsReadOnly}
+      />
+    </form>
+  );
+}
