9503fc5c88bd9acfbbf1a17a637c130010646c2b
FuuKowatty <miedzio.2000@gmail.com>
2023-07-24 15:41:06 +0200

    update domain

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -4,6 +4,6 @@ const isProduction = process.env.NODE_ENV !== "development";
 
 export const apiClient = axios.create({
   baseURL: isProduction
-    ? "taskmanager-bapmj788y-fuukowatty.vercel.app/api/"
+    ? "taskmanager-ruddy-xi.vercel.app/api/"
     : "http://localhost:3000/api/",
 });
