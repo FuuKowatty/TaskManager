2806f268b3a26c4417d554d17617f9b60023c333
FuuKowatty <miedzio.2000@gmail.com>
2023-07-24 14:30:37 +0200

    restart prisma connection

--- prisma/migrations/20230706182945_add_start_and_end_dates/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `endDate` to the `Task` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Task" ADD COLUMN     "endDate" TIMESTAMP(3) NOT NULL,
-ADD COLUMN     "startDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP;
--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -4,6 +4,6 @@ const isProduction = process.env.NODE_ENV !== "development";
 
 export const apiClient = axios.create({
   baseURL: isProduction
-    ? "http://localhost:3000/api/"
+    ? "https://task-manager-gamma-seven.vercel.app/"
     : "http://localhost:3000/api/",
 });
