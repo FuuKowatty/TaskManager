77b170bc9ec8b052a0227566b56eae2b6e3f5408
Maciej Garncarski <maciejgarncarski@protonmail.com>
2023-08-04 10:32:43 +0200

    fix: semantic html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,83 +1,38 @@
-"use client";
-
 import Image from "next/image";
 
+import { ButtonDemo } from "@/components/button/ButtonDemo";
 import { RedirectButton } from "@/components/button/ButtonRedirect";
 
-import { useLoginDemo } from "@/hooks/api/useLoginDemo";
+import cover from "./cover.webp";
 
 export default function Home() {
-  const { handleLoginDemo } = useLoginDemo();
-
   return (
-    <main
-      className={"flex h-screen flex-1 flex-col dark:bg-darkGray md:flex-row"}
-    >
-      <div className={"flex w-full flex-1 flex-col justify-center space-y-12"}>
-        <div
-          className={
-            "flex flex-1 flex-col items-center gap-5 pb-10 lg:flex-row lg:gap-0 lg:pb-0"
-          }
-        >
-          <div className={"flex flex-col space-y-8 px-8 lg:w-7/12"}>
-            <div className={"flex justify-center lg:justify-start"}></div>
-            <h1
-              className={
-                "flex flex-col text-center  text-3xl font-bold lg:text-left xl:text-5xl 2xl:text-6xl"
-              }
-            >
-              <span className="">Task Manager App</span>
-
-              <span className="dark:text-white">
-                for personal task management
-              </span>
-            </h1>
-
-            <div
-              className={
-                "text-center text-base text-gray-500 dark:text-gray-300 lg:text-left lg:text-lg xl:text-xl " +
-                " font-heading flex flex-col space-y-1.5 md:w-full"
-              }
-            >
-              <span>TaskManager is a powerful app designed for teams</span>
-
-              <span>
-                allowing admins to manage users, managers to assign tasks
-              </span>
-
-              <span>and employees to track their progress.</span>
-            </div>
-
-            <div
-              className={
-                "flex flex-col items-center space-y-2 lg:flex-row lg:space-x-4 lg:space-y-0"
-              }
-            >
-              <RedirectButton>Login</RedirectButton>
-              <button
-                onClick={() => handleLoginDemo()}
-                type="button"
-                className="w-[150px] rounded-md bg-blue-700 px-8 py-4 text-center font-bold text-white hover:bg-blue-800 dark:bg-gray-600 dark:hover:bg-gray-700"
-              >
-                View Demo
-              </button>
-            </div>
-          </div>
-
-          <div
-            className={
-              "relative -order-10 h-full w-full justify-end lg:order-last lg:flex lg:w-5/12"
-            }
-          >
-            <Image
-              fill
-              className={"absolute right-0 top-0 object-cover"}
-              src={"https://i.imgur.com/MEmZlmW.jpg"}
-              alt={`Customer Support image`}
-            />
-          </div>
+    <main className="flex h-screen flex-1 flex-col items-center gap-5 pb-10 dark:bg-darkGray md:flex-row lg:gap-0 lg:pb-0">
+      <section className="flex flex-col space-y-8 px-8 lg:w-7/12">
+        <h1 className="flex flex-col text-center text-3xl font-bold lg:text-left xl:text-5xl 2xl:text-6xl">
+          <span>Task Manager App</span>
+          <span className="dark:text-white">for personal task management</span>
+        </h1>
+        <p className="font-heading flex flex-col space-y-1.5 text-center text-base text-gray-500 dark:text-gray-300 md:w-full lg:text-left lg:text-lg xl:text-xl">
+          <span>TaskManager is a powerful app designed for teams</span>
+          <span>allowing admins to manage users, managers to assign tasks</span>
+          <span>and employees to track their progress.</span>
+        </p>
+        <div className="flex flex-col items-center space-y-2 lg:flex-row lg:space-x-4 lg:space-y-0">
+          <RedirectButton>Login</RedirectButton>
+          <ButtonDemo />
         </div>
-      </div>
+      </section>
+
+      <figure className="relative -order-10 h-full w-full justify-end lg:order-last lg:flex lg:w-5/12">
+        <Image
+          fill
+          className={"absolute right-0 top-0 object-cover"}
+          src={cover}
+          alt={`Customer Support image`}
+          placeholder="blur"
+        />
+      </figure>
     </main>
   );
 }
